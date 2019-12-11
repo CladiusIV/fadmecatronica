64ccb2e05605651d74c216f8d3778ef9a2668c7c
Claudio <claudioslopez04@gmail.com>
2019-12-11 11:15:23 -0300

    Updates

---
+++ precache-manifest.917e86acf276c71b78810af4c89361d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9453ab7530de7027f2a8bae7134956",
+    "url": "/fadmecatronica/index.html"
+  },
+  {
+    "revision": "dd695c273dab073a4db5",
+    "url": "/fadmecatronica/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5851e8663b70edc07d29",
+    "url": "/fadmecatronica/static/js/2.9f5bb4f0.chunk.js"
+  },
+  {
+    "revision": "dd695c273dab073a4db5",
+    "url": "/fadmecatronica/static/js/main.5f7456fb.chunk.js"
+  },
+  {
+    "revision": "5d64b6569d001a531798",
+    "url": "/fadmecatronica/static/js/runtime-main.3031bfb8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/fadmecatronica/static/media/logo.25bf045c.svg"
+  }
+]);
